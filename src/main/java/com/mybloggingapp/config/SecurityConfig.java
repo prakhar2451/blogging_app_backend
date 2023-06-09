3f4e145e36b5857832ad346c93b36aaf129c5271
prakhar2451 <prakhar.007.pk@gmail.com>
2023-06-09 13:01:00 +0530

    Implemented basic security for all the API . Authentication based on database stored credentials

--- src/main/java/com/mybloggingapp/config/SecurityConfig.java
+++ src/main/java/com/mybloggingapp/config/SecurityConfig.java
@@ -7,14 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends com.mybloggingapp.configuration.WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailService customUserDetailService;
--- src/main/java/com/mybloggingapp/repositories/UserRepo.java
+++ src/main/java/com/mybloggingapp/repositories/UserRepo.java
@@ -2,5 +2,8 @@ package com.mybloggingapp.repositories;
 import com.mybloggingapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Integer> {
+    Optional<Object> findByEmail(String username);
 }
