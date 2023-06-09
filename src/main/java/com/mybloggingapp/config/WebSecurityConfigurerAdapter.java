9b6da402da4a1015559de3c0b39efe3c6a64ce5f
prakhar2451 <prakhar.007.pk@gmail.com>
2023-06-09 12:24:14 +0530

    Implemented basic security for all the API . Authentication based on database stored credentials

---
+++ src/main/java/com/mybloggingapp/config/WebSecurityConfigurerAdapter.java
@@ -0,0 +1,9 @@
+package com.mybloggingapp.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+public class WebSecurityConfigurerAdapter {
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+}
